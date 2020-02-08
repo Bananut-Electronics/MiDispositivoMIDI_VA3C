17c0ed82b90d1e26c02521e9ef9b6b4eeb02960b
4dvn <vuha.hau@hyper.vn>
2020-02-08 22:36:36 +0700

    Update MDM_V3_Library4x4.ino
    Ready to merge

--- examples/MDM_V3_Library4x4/MDM_V3_Library4x4.ino
+++ examples/MDM_V3_Library4x4/MDM_V3_Library4x4.ino
@@ -22,34 +22,11 @@
 // Include the library
 #include <MiDispositivoMIDI_V3.h>
 
-// Creates a mdm with four pages and only one expansion
 BoardRoles bRoles[1] = {TOUCHPAD};
 MiDispositivoMIDI_V3 mdm = MiDispositivoMIDI_V3(SINGLE_DEVICE_4X4, bRoles);
 
-// Couple of variables for the led animation
-int buttonCounter    = 0;
-int expansionCounter = 0;
-bool myflag          = 0;
-
-
 void setup() {
-
-  // Assign MIDI notes to buttons. Note that if you are using
-  // more than one expansion (more than one 4x4 device) you will
-  // need to change the size of this array. Always 1 value per button.
-  uint8_t midiNotes [1][16] = {{ 36, 37, 38, 39,
-                                 40, 41, 42, 43,
-                                 44, 45, 46, 47,
-                                 48, 49, 50, 51}};
-  mdm.setMidiNotes(midiNotes);
-
-  // Same with the so called velocities of the notes. This means the
-  // intensity of each note.
-  uint8_t midiVelocities [1][16] = {{127,127,127,127,
-                                    127,127,127,127,
-                                    127,127,127,127,
-                                    127,127,127,127}};
-
+  
   mdm.setMidiVeloc(midiVelocities);
   // Configure the colors when the LED is not pressed
   uint8_t offColors[3] = {255, 0, 0};
@@ -63,38 +40,16 @@ void setup() {
   mdm.setInterrupt(doFunnyAnimation, 0.025);
 }
 
-void loop() {
-  // Run the loop
+void loop() {  
+  // Run the loop. This will read all buttons and
+  // execute different actions, such as light up the
+  // pressed LEDs, and send a MIDI noteOn message.
   mdm.loop();
 }
 
 // Function that is called periodically
 void doFunnyAnimation(){
-  
+
   // This animation wipes between two colors
   uint8_t colores1[3] = {0, 50, 0};
-  uint8_t colores2[3] = {0, 50, 50};
-
-  mdm.setPixelColor(expansionCounter, buttonCounter, myflag ? colores1 : colores2);
-
-  // Generates the following sequence.
-  //      for (int i = 0; i < 4; i++)
-  //        for (int j = 0; j < 16; j++)
-  
-  if (buttonCounter == 15)
-  {
-    buttonCounter = 0;
-    if (expansionCounter == 1)
-    {
-      expansionCounter = 0;
-      myflag = !myflag;
-    }
-    else{
-      expansionCounter += 1;
-    }
-  }
-  else
-  {
-    buttonCounter += 1;
-  }
-}
+};
